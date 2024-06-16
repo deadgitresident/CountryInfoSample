a1192abe324dc58f72e3c6d60f6c25199b93c570
ProfessorGeng <153098583+kazelegendo@users.noreply.github.com>
2024-06-16 14:31:27 +0800

    Add files via upload

---
+++ CountryInfo.py
@@ -0,0 +1,27 @@
+from countryinfo import CountryInfo
+
+country = CountryInfo(input("China"))
+
+
+
+# 國家資訊
+
+print("Country Name:", country.name())
+
+print("Capital:", country.capital())
+
+print("Population:", country.population())
+
+print("Area (in square kilometers):", country.area())
+
+print("Region:", country.region())
+
+print("Subregion:", country.subregion())
+
+print("Demonym:", country.demonym())
+
+print("Currency:", country.currencies())
+
+print("Languages:", country.languages())
+
+print("Borders: ", country.borders())
